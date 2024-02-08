35bee0bfadf438c41034f774a8d6f5da6d78166c
Yxel._14 <jtalleda@insgabrielamistral.cat>
2024-02-08 16:44:01 +0100

    3a modificacio git

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("1a modificacio git");
+        System.out.println("2a modificació GitHub");
+        System.out.println("3a modificació Git");
     }
 }
